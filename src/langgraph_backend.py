a78e01a3fb9fa35da1b2fee901ba304739b74ec1
NayeemHossenJim <nayeemhossenjim@gmail.com>
2026-06-02 10:15:04 +0600

    Refactor chatbot backend to use SQLite for state management and enhance Streamlit frontend with threading support

--- src/langgraph_backend.py
+++ src/langgraph_backend.py
@@ -1,15 +1,14 @@
+import sqlite3
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, START, END
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain_core.messages import BaseMessage, HumanMessage
 
 load_dotenv()
-
 llm = ChatOpenAI()
-
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
@@ -18,8 +17,8 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
-# Checkpointer
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -27,3 +26,10 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
